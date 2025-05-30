850b25219bff25a45e64dad5b78b9a0a9445c6e8
Prathamesh Gurav <prathamesh2005gurav@gmail.com>
2025-05-30 19:39:02 +0530

    complete project setup and basic route ok status

--- src/app.js
+++ src/app.js
@@ -8,13 +8,26 @@ app.use(cors(
         credentials:true,
     }
 ));
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// routes  import 
+
+import userRouter from './routes/user.routes.js'
+
+
+// routes declaration
+app.use("/api/v1/users",userRouter);
 
 
 
+//http://localhost:3000/api/v1/user/login
 
 export default app;
